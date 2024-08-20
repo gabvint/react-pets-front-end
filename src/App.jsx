a9a6ce8cce01863178b5f2aeb431b142a0edda21
gabtongol <geybtongol@gmail.com>
2024-08-20 13:49:07 -0400

    rendered pet list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 // src/App.jsx
 import { useState , useEffect} from "react";
-
+import * as petService from '../services/petService.js';
+import PetList from '../components/PetList.jsx';
 
 const App = () => {
   const [petList, setPetList] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => {
+  
+      const fetchPets = async () => {
+        try {
+          const pets = await petService.index()
+          if (pets.error){
+            throw new Error(pets.error);
+          }
+
+          setPetList(pets)
+
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      fetchPets()
+  }, []) 
+
+  const updateSelected = (pet) => {
+    setSelected(pet)
+    console.log(pet)
+  } 
 
   return(
     <>
       <h1>Hello world!</h1>
+      <PetList petList={petList} updateSelected={updateSelected} />
     </>
   ) 
     
